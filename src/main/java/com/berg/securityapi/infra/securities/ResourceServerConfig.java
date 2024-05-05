b2620c5838565276ad204882606e46f7caeb3e64
ulremberg <berguebarboza@hotmail.com>
2024-05-05 13:48:39 -0300

    added EnableMethodSecurity instead EnableGlobalMethodSecurity

--- src/main/java/com/berg/securityapi/infra/securities/ResourceServerConfig.java
+++ src/main/java/com/berg/securityapi/infra/securities/ResourceServerConfig.java
@@ -1,37 +1,30 @@
 package com.berg.securityapi.infra.securities;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class ResourceServerConfig  {
+@EnableMethodSecurity
+public class ResourceServerConfig {
 
     private final KeycloakJwtAuthenticationConverter keycloakJwtAuthenticationConverter;
-    
+
     @SuppressWarnings("removal")
     @Bean
-   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                . authorizeHttpRequests(authz -> authz.requestMatchers("/security/**").authenticated())
-                . oauth2ResourceServer(server -> server
+                .authorizeHttpRequests(authz -> authz.requestMatchers("/security/**").authenticated())
+                .oauth2ResourceServer(server -> server
                         .jwt().jwtAuthenticationConverter(keycloakJwtAuthenticationConverter));
-       return http.build();
-   }
+        return http.build();
+    }
 
-    // @Override
-    // public void configure(final HttpSecurity http) throws Exception {
-    //     http
-    //             .authorizeRequests(authz -> authz.requestMatchers("/security/**").authenticated())
-    //             .oauth2ResourceServer()
-    //             .jwt().jwtAuthenticationConverter(keycloakJwtAuthenticationConverter);
-    // }
 }
